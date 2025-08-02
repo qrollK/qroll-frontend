8d02e845e5fbab77f016d59bb487891de86fb420
qrollK <itskrish2654@gmail.com>
2025-08-02 14:51:28 +0000

    Moved project files to root directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import AdminDashboard from './pages/AdminDashboard';
-import StudentDashboard from './pages/StudentDashboard';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
   return (

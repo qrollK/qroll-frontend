bec75fbb8401776ec3baf2190f1674bca8cf2e69
qrollK <itskrish2654@gmail.com>
2025-08-03 18:59:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import StudentDashboard from "./pages/StudentDashboard";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from "react";
 
 export default function App() {
   return (
-    <GoogleOAuthProvider clientId="your-google-client-id">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/student" element={<StudentDashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <div>
+      <h1>Qroll App is Running</h1>
+      <p>If you see this, React is working fine!</p>
+    </div>
   );
 }

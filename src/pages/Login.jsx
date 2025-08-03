0be748d411e7b2a5a523dbe5e2e055c0edd2fed7
qrollK <itskrish2654@gmail.com>
2025-08-03 14:42:00 +0000

    Moved project files to root directory

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
-import jwt_decode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const Login = () => {
   const handleSuccess = async (credentialResponse) => {

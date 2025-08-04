beaf0df3deeec5dc4ed5296bf23820617e6e9d58
qrollK <itskrish2654@gmail.com>
2025-08-04 19:43:47 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,15 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // match key from login.jsx
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;

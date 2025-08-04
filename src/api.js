72c2ef2e0faadeaf3cbdaf9afaf602eabd8355ce
qrollK <itskrish2654@gmail.com>
2025-08-04 17:21:56 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL + '/api', // e.g., https://qroll-backend.up.railway.app/api
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use((config) => {
